5c1cbaf15ab0be9e592800f8e919a67253cdd138
Nanuela <miobledor@gmail.com>
2025-01-29 11:39:59 -0300

    .

--- lista-exercicios/atividade-2.js
+++ lista-exercicios/atividade-2.js
@@ -1,66 +1,34 @@
-let num1 = 10; 
+let num1 = 10;
 let num2 = 20; 
 
-if (num1 > num2) {
-    console.log("o primeiro número é maior que o segundo.");
-    }
-else if (num1 <= num2) {
-    console.log("o primeiro número é maior ou igual ao segundo.");
-}
-if (num1 == num2) {
-    console.log("os dois números são iguais.");
-}
+console.log(num1 > num2 ? "O primeiro número é maior que o segundo." : ""); 
+console.log(num1 < num2 ? "O primeiro número é menor que o segundo." : ""); 
+console.log(num1 === num2 ? "Os dois números são iguais." : "");
 
 console.log("----------------")
-
 let idade = 18; 
 
-if (idade >= 16) { 
- console.log("você tem idade para votar."); }  
-if (idade >= 18 == idade  70) {
-    console.log("você é obrigado a votar."); }
+console.log(idade >= 16 ?"Você tem idade para votar." : ""); 
+console.log(idade >= 18 && idade <= 70 ? "Você é obrigado a votar." : "")
 
 console.log("----------------")
-
-if (idade >= 18) { 
-    console.log("você é maior de idade."); }
-if (idade >= 60) { 
-    console.log("você é idoso."); } 
+console.log(idade >= 18 ? "Você é maior de idade." : ""); 
+console.log(idade >= 60 ? "Você é idoso." : "");
 
 console.log("----------------")
-
 let nomeUsuario = "admin"; 
 
-if (nomeUsuario == "admin") { 
-    console.log("login bem-sucedido,o usuário é admin");}
-else { 
- console.log("usuário comum."); } 
-
- console.log("----------------")
-
- let numero = 15;
-
- if (numero % 3 == 0) { 
-  console.log("o número é divisível por 3."); }  
- if (numero % 5 == 0) { 
-  console.log("o número é divisível por 5."); } 
+console.log(nomeUsuario === "admin" ? "Login bem-sucedido. Usuário é admin." : "Usuário comum.");
 
 console.log("----------------")
+let numero = 15; 
 
+console.log(numero % 3 === 0 ? "O número é divisível por 3." : ""); 
+console.log(numero % 5 === 0 ? "O número é divisível por 5." : "");
+
+console.log("----------------")
 let saldo = 500; 
 let saque = 200; 
 
-if (saldo >= saque) { 
- console.log("saque realizado com sucesso."); saldo - saque} 
-
-else { 
- console.log("saldo insuficiente."); } 
- 
-if (saldo > 100) { 
- console.log("o saldo após o saque é maior que r$100."); } 
-
-else { 
- console.log("o saldo após o saque é inferior a r$100."); }
-
- 
-
+console.log(saldo >= saque ? "Saque realizado com sucesso." : "Saldo insuficiente."); saldo -= saque; 
+console.log(saldo > 100 ? "O saldo após o saque é maior que R$100." : "O saldo após o saque é inferior a R$100.");
