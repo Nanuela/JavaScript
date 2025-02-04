7b1ee43a2c606c1d0a65325749878f8afab08d7a
Nanuela <miobledor@gmail.com>
2025-02-04 09:01:46 -0300

    .

--- estrutura-condicional/if.js
+++ estrutura-condicional/if.js
@@ -8,3 +8,24 @@ if (idade >= 18) {
 } else {
     console.log("você é menor de idade")
 }
+
+console.log("----------------")
+// verificando se um número é impar ou par 
+
+let numero = prompt("escolha um número:")
+let resultado = numero % 2 //resto
+if (resultado == 0) {
+    console.log("o número é par")
+} else {
+    console.log("o número é impar")
+}
+
+console.log("----------------")
+//avaliação do aluno 
+
+let nota1 = prompt("quala primeira nota:")
+let nota2 = prompt("qual a segunda nota:")
+let total = (nota1 + nota2) / 2 
+if (total >= 7)  {
+    
+}
