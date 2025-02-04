6e047975c9661a5e479977f419bdd6f9951d161a
Nanuela <miobledor@gmail.com>
2025-02-04 11:28:12 -0300

    .

---
+++ lista-exercicios/atividade-4.js
@@ -0,0 +1,102 @@
+let idade = prompt("qual a sua idade?")
+if (idade < 12) {
+    console.log("criança")
+} else if (idade < 17) {
+    console.log("adolescente")
+} else if (idade < 59) {
+    console.log("adulto")
+} else {
+    console.log("idoso")
+}
+
+console.log("----------------")
+let temperatura = prompt("qual a temperatura em °C?")
+if (temperatura < 10) {
+    console.log("muito frio")
+} else if (temperatura < 20) {
+    console.log("frio")
+} else if (temperatura < 30) {
+    console.log("agradável")
+} else {
+    console.log("muito quente")
+}
+
+console.log("muito uente")
+let turno = prompt("qual o turno de estudo?")
+if (turno == "M") {
+    console.log("bom dia!")
+} else if (turno == "V") {
+    console.log("boa tarde!")
+} else if (turno == "N") {
+    console.log("boa noite!")
+} else {
+    console.log("turno inválido")
+}
+
+console.log("muito uente")
+let valor = prompt("qual o valor da compra?")
+if (valor <= 100) {
+    console.log("desconto de 5%")
+} else if (valor <= 200) {
+    console.log("desconto de 10%")
+} else {
+    console.log("desconto de 15%")
+}
+
+console.log("muito uente")
+let nota = prompt("qual a sua nota?")
+if (nota > 90) {
+    console.log("A")
+} else if (nota > 80) {
+    console.log("B")
+} else if (nota > 70) {
+    console.log("C")
+} else if (nota > 60) {
+    console.log("D")
+} else {
+    console.log("F")
+}
+
+console.log("muito uente")
+let dia = prompt("digite um número de 1 a 7:")
+if (dia = 1){
+    console.log("domingo")
+} else if (dia = 2){
+    console.log("segunda")
+} else if (dia = 3){
+    console.log("terça")
+} else if (dia = 4){
+    console.log("quarta")
+} else if (dia = 5){
+    console.log("quinta")
+} else if (dia = 6){
+    console.log("sexta")
+} else if (dia = 7){
+    console.log("sabádo")
+} else {
+    console.log("inválido")
+}
+
+console.log("muito uente")
+let distancia = prompt("qual a distância da pista em metros?")
+if (distancia < 400) {
+    console.log("pista curta")
+} else if (distancia < 800) {
+    console.log("pista média")
+} else if (distancia < 1500) {
+    console.log("pista longa")
+} else {
+    console.log("psta muito longa")
+}
+
+console.log("muito uente")
+let pontuacao = prompt("qual a sua pontuação?")
+if (pontuacao < 1000) {
+    console.log("iniciante")
+} else if (pontuacao < 5000) {
+    console.log("intermediario")
+} else if (pontuacao < 10000) {
+    console.log("avançado")
+} else {
+    console.log("mestre")
+}
