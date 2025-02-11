c8ab07a3479ffce1a3e92c7f9fe18abcc5d1d171
Nanuela <miobledor@gmail.com>
2025-02-11 09:25:49 -0300

    .

--- estrutura-condicional/while.js
+++
@@ -1,8 +0,0 @@
-let contador = 0 
-//inicia a contagem em zero
-while(contador <= 5) {
-//continua enquanto a condição for verdade 
-    console.log(contador) //mostra o num atual 
-    contador++ //atualiza o valor do contador 
-    
-}
---
+++ lista-exercicios/atividade-6.js
@@ -0,0 +1,107 @@
+let numero = 3
+switch (numero) {
+case 1:
+    console.log("domingo")
+    break
+case 2:
+    console.log("segunda-feira")
+    break
+case 3:
+    console.log("terça-feira")
+    break
+case 4:
+    console.log("quarta-feira")
+    break
+case 5:
+    console.log("quinta-feira")
+    break
+case 6:
+    console.log("sexta-feira")
+    break
+case 7:
+    console.log("sábado")
+    break
+default:
+    console.log("número inválido. Insira um valor entre 1 e 7.")
+}
+
+console.log("----------------")
+let idade = 15
+switch (idade) {
+case 5:
+    console.log("infantil A")
+    break
+case 10:
+    console.log("infantil B")
+    break
+case 15:
+    console.log("juvenil A")
+    break
+case 20:
+    console.log("juvenil B")
+    break
+case 30:
+    console.log("adulto")
+    break
+default:
+    console.log("idade inválida. Insira 5, 10, 15, 20 ou 30.")
+}
+
+console.log("----------------")
+let turno = "V"
+switch (turno) {
+case "M":
+    console.log("bom dia!")
+    break
+case "V":
+    console.log("boa tarde!")
+    break
+case "N":
+    console.log("boa noite!")
+    break
+default:
+    console.log("turno inválido. Insira M, V ou N.")
+}
+
+console.log("----------------")
+let numero1 = 4
+switch (numero1) {
+case 1:
+    console.log("primeira mensagem")
+    break
+case 2:
+    console.log("segunda mensagem")
+    break
+case 3:
+    console.log("terceira mensagem")
+    break
+case 4:
+    console.log("quarta mensagem")
+    break
+case 5:
+    console.log("quinta mensagem")
+    break
+default:
+    console.log("número fora do intervalo. Insira um valor entre 1 e 5.")
+}
+
+
+console.log("----------------")
+let estacao = 2
+switch (estacao) {
+case 1:
+    console.log("primavera")
+    break
+case 2:
+    console.log("verão")
+    break
+case 3:
+    console.log("outono")
+    break
+case 4:
+    console.log("inverno")
+    break
+default:
+    console.log("estação inválida. Insira um número de 1 a 4.")
+}
+
