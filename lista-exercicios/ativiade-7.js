f387cd40614dd49f7f5a750b9e096af71e848e9b
Nanuela <miobledor@gmail.com>
2025-02-11 10:49:42 -0300

    .

---
+++ lista-exercicios/ativiade-7.js
@@ -0,0 +1,56 @@
+let contador = 1 
+while (contador <= 10) {
+    console.log(contador) 
+    console++
+}
+
+console.log("----------------")
+let numero1 = prompt("Escolha um número de 1 a 10:")
+let mutiplicador = 1 
+while (mutiplicador <= 10) {
+    let resultado = numero1 * mutiplicador
+    console.log(numero1, "x", mutiplicador, "=", resultado)
+    mutiplicador += 1
+}
+
+console.log("----------------")
+let contador1 = 10 
+while (contador >= 1) {
+    console.log(contador1) 
+    console++
+}
+
+console.log("----------------")
+let palavra = prompt("Digite a senha")
+while (palavra != "12345") {
+    palavra =  prompt("Tente novamente: ")
+    if (palavra == 12345) {
+        console.log("Acertou!")
+    }
+}
+
+console.log("----------------")
+let contador2 = 50 
+while (contador >= 100) {
+    console.log(contador2) 
+    console++
+}
+
+console.log("----------------")
+let numero2 = 0
+while (numero2 <= 100) {
+  console.log(numero2)
+  numero2 += 5
+}
+
+console.log("----------------")
+let repeticao = 0
+while (repeticao < 5) {
+  console.log("Eu gosto de JavaScript")
+  repeticao++
+}
+
+console.log("----------------")
+let nome = prompt("Digete seu nome:")
+
+
