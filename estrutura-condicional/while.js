cfe7bdf542def58f57841cfd6e30f73db174b843
Nanuela <miobledor@gmail.com>
2025-02-11 08:34:38 -0300

    .

---
+++ estrutura-condicional/while.js
@@ -0,0 +1,8 @@
+let contador = 0 
+//inicia a contagem em zero
+while(contador <= 5) {
+//continua enquanto a condição for verdade 
+    console.log(contador) //mostra o num atual 
+    contador++ //atualiza o valor do contador 
+    
+}
