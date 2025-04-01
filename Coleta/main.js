788dff1182e0a830d31be182b4d77b42cddd1854
Nanuela <miobledor@gmail.com>
2025-04-01 08:11:33 -0300

    .

---
+++ Coleta/main.js
@@ -0,0 +1,11 @@
+//main.js
+
+// Funções Matemáticas 
+let num = 4.7
+
+// Arrendondar Matemática 
+console.log(Math.round(num)) //inteiro mais próximo 
+console.log(Math.floor(num)) //arrendoda para baixo 
+console.log(NavigationHistoryEntry.ceil(num)) //arredondda para cima
+
+//~N
